a81eea8ca40c10c5cec1beac8924218f8835998e
eliyaomid <eliyaomid@gmail.com>
2017-08-28 21:48:35 +0430

    Add template tpl files

--- code/sites/all/themes/tiam/template.php
+++ code/sites/all/themes/tiam/template.php
@@ -4,33 +4,33 @@
  * Page alter.
  */
 function tiam_page_alter($page) {
-	$mobileoptimized = array(
-		'#type' => 'html_tag',
-		'#tag' => 'meta',
-		'#attributes' => array(
-		'name' =>  'MobileOptimized',
-		'content' =>  'width'
-		)
-	);
-	$handheldfriendly = array(
-		'#type' => 'html_tag',
-		'#tag' => 'meta',
-		'#attributes' => array(
-		'name' =>  'HandheldFriendly',
-		'content' =>  'true'
-		)
-	);
-	$viewport = array(
-		'#type' => 'html_tag',
-		'#tag' => 'meta',
-		'#attributes' => array(
-		'name' =>  'viewport',
-		'content' =>  'width=device-width, initial-scale=1'
-		)
-	);
-	drupal_add_html_head($mobileoptimized, 'MobileOptimized');
-	drupal_add_html_head($handheldfriendly, 'HandheldFriendly');
-	drupal_add_html_head($viewport, 'viewport');
+    $mobileoptimized = [
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => [
+            'name' =>  'MobileOptimized',
+            'content' =>  'width'
+        ]
+    ];
+    $handheldfriendly = [
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => [
+            'name' =>  'HandheldFriendly',
+            'content' =>  'true'
+        ]
+    ];
+    $viewport = [
+        '#type' => 'html_tag',
+        '#tag' => 'meta',
+        '#attributes' => [
+            'name' =>  'viewport',
+            'content' =>  'width=device-width, initial-scale=1'
+        ]
+    ];
+    drupal_add_html_head($mobileoptimized, 'MobileOptimized');
+    drupal_add_html_head($handheldfriendly, 'HandheldFriendly');
+    drupal_add_html_head($viewport, 'viewport');
 }
 
 
@@ -47,26 +47,26 @@ function tiam_preprocess_html(&$variables) {
  */
 function tiam_preprocess_page(&$variables) {
 
-	/**
-	 * insert variables into page template.
-	 */
-	if($variables['page']['sidebar_first'] && $variables['page']['sidebar_second']) { 
-		$variables['sidebar_grid_class'] = 'col-md-3';
-		$variables['main_grid_class'] = 'col-md-6';
-	} elseif ($variables['page']['sidebar_first'] || $variables['page']['sidebar_second']) {
-		$variables['sidebar_grid_class'] = 'col-md-4';
-		$variables['main_grid_class'] = 'col-md-8';		
-	} else {
-		$variables['main_grid_class'] = 'col-md-12';			
-	}
+    /**
+     * insert variables into page template.
+     */
+    if($variables['page']['sidebar_first'] && $variables['page']['sidebar_second']) { 
+        $variables['sidebar_grid_class'] = 'col-md-3';
+        $variables['main_grid_class'] = 'col-md-6';
+    } elseif ($variables['page']['sidebar_first'] || $variables['page']['sidebar_second']) {
+        $variables['sidebar_grid_class'] = 'col-md-4';
+        $variables['main_grid_class'] = 'col-md-8';		
+    } else {
+        $variables['main_grid_class'] = 'col-md-12';			
+    }
 
-	if($variables['page']['header_top_left'] && $variables['page']['header_top_right']) { 
-		$variables['header_top_left_grid_class'] = 'col-md-8';
-		$variables['header_top_right_grid_class'] = 'col-md-4';
-	} elseif ($variables['page']['header_top_right'] || $variables['page']['header_top_left']) {
-		$variables['header_top_left_grid_class'] = 'col-md-12';
-		$variables['header_top_right_grid_class'] = 'col-md-12';		
-	}	
+    if($variables['page']['header_top_left'] && $variables['page']['header_top_right']) { 
+        $variables['header_top_left_grid_class'] = 'col-md-8';
+        $variables['header_top_right_grid_class'] = 'col-md-4';
+    } elseif ($variables['page']['header_top_right'] || $variables['page']['header_top_left']) {
+        $variables['header_top_left_grid_class'] = 'col-md-12';
+        $variables['header_top_right_grid_class'] = 'col-md-12';		
+    }	
 }
 
 
@@ -75,7 +75,7 @@ function tiam_preprocess_page(&$variables) {
  * Preprocess variables for block.tpl.php
  */
 function tiam_preprocess_block(&$variables) {
-	$variables['classes_array'][]='clearfix';
+    $variables['classes_array'][]='clearfix';
 }
 
 
@@ -86,20 +86,20 @@ function tiam_preprocess_block(&$variables) {
  * Print breadcrumbs as a list, with separators.
  */
 function tiam_breadcrumb($variables) {
-	$breadcrumb = $variables['breadcrumb'];
+    $breadcrumb = $variables['breadcrumb'];
 
-	if (!empty($breadcrumb)) {
-		$breadcrumb[] = drupal_get_title();
-		$breadcrumbs = '<ol class="breadcrumb">';
+    if (!empty($breadcrumb)) {
+            $breadcrumb[] = drupal_get_title();
+            $breadcrumbs = '<ol class="breadcrumb">';
 
-		$count = count($breadcrumb) - 1;
-		foreach ($breadcrumb as $key => $value) {
-		$breadcrumbs .= '<li>' . $value . '</li>';
-		}
-		$breadcrumbs .= '</ol>';
+            $count = count($breadcrumb) - 1;
+            foreach ($breadcrumb as $key => $value) {
+            $breadcrumbs .= '<li>' . $value . '</li>';
+            }
+            $breadcrumbs .= '</ol>';
 
-		return $breadcrumbs;
-	}
+            return $breadcrumbs;
+    }
 }
 
 
@@ -108,14 +108,14 @@ function tiam_breadcrumb($variables) {
  * Search block form alter.
  */
 function tiam_form_alter(&$form, &$form_state, $form_id) {
-	if ($form_id == 'search_block_form') {
-	    unset($form['search_block_form']['#title']);
-	    $form['search_block_form']['#title_display'] = 'invisible';
-		$form_default = t('Search this website...');
-	    $form['search_block_form']['#default_value'] = $form_default;
+    if ($form_id == 'search_block_form') {
+        unset($form['search_block_form']['#title']);
+        $form['search_block_form']['#title_display'] = 'invisible';
+            $form_default = t('Search this website...');
+        $form['search_block_form']['#default_value'] = $form_default;
 
-		$form['actions']['submit']['#attributes']['value'][] = '';
+            $form['actions']['submit']['#attributes']['value'][] = '';
 
-	 	$form['search_block_form']['#attributes'] = array('onblur' => "if (this.value == '') {this.value = '{$form_default}';}", 'onfocus' => "if (this.value == '{$form_default}') {this.value = '';}" );
-	}
+            $form['search_block_form']['#attributes'] = ['onblur' => "if (this.value == '') {this.value = '{$form_default}';}", 'onfocus' => "if (this.value == '{$form_default}') {this.value = '';}" ];
+    }
 }
